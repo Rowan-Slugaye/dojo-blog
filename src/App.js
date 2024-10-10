76713f941e8ac0298e77342f0f4f3292bd157e86
Rowan-Slugaye <83877730+Rowan-Slugaye@users.noreply.github.com>
2024-10-10 22:44:54 +0200

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-
   return (
     <div className="App">
       <div className="content">
-        <h1>{ title }</h1>
-        <p>{ likes }</p>
+        <h1>App Component</h1>
       </div>
     </div>
   );
