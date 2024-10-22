12d4873f19cce96f1c0ab2bdadcf79af1c922d70
Rowan-Slugaye <83877730+Rowan-Slugaye@users.noreply.github.com>
2024-10-22 16:25:32 +0200

    modified:   src/home.js

--- src/home.js
+++ src/home.js
@@ -3,17 +3,27 @@ import BlogList from "./BlogList";
 
 const Home = () => {
 
-    const [blogs, setblogs] = useState(null)
-    const [isPending, setIsPending] = useState(true)
+    const [blogs, setblogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
       setTimeout(() => {
       fetch('http://192.168.86.166:8000/blogs') // I didnt use http://localhost:8000/blogs becuse i wanted it to work on other Computers 
-       .then(res => {
-        return res.json();
-      })
+        .then(res => {
+          if(!res.ok){
+            throw Error('Could not fetch the data from that resource');
+
+          }
+          return res.json();
+        })
         .then((data) => {
           setblogs(data);
+          setIsPending(false);
+          setError(false);
+        })
+        .catch(err => {
+          setError(err.message);
           setIsPending(false);
         })
       }, 1000);
@@ -21,6 +31,7 @@ const Home = () => {
 
     return ( 
         <div className="home">
+          {error && <div>{ error }</div>}
           { isPending && <div>Loading...</div>}
           {blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
         </div>
