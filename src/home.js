d677909019f4e6c06b9fccfcc34dde14437ef058
Rowan-Slugaye <83877730+Rowan-Slugaye@users.noreply.github.com>
2024-10-22 15:56:50 +0200

    EP 17 finshed

--- src/home.js
+++ src/home.js
@@ -3,24 +3,22 @@ import BlogList from "./BlogList";
 
 const Home = () => {
 
-    const [blogs, setblogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-      ])
-
-    const handleDelete = (id) => {
-      const newBlogs = blogs.filter(blog => blog.id !== id);
-      setblogs(newBlogs)
-    }
+    const [blogs, setblogs] = useState(null)
 
     useEffect(() => {
       console.log('use effect ran')
+      fetch('http://192.168.86.166:8000/blogs')
+       .then(res => {
+        return res.json()})
+        .then((data) => {
+          console.log(data)
+          setblogs(data)
+        })
     }, []);
 
     return ( 
         <div className="home">
-          <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+          {blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
         </div>
      );
 }
