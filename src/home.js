ad4b4df563c6c2c54661ab584f7fb856dff211ad
Rowan-Slugaye <83877730+Rowan-Slugaye@users.noreply.github.com>
2024-10-22 15:40:27 +0200

    EP 16 finshed

--- src/home.js
+++ src/home.js
@@ -9,8 +9,6 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
       ])
 
-      const [name, setName] = useState('Mario')
-
     const handleDelete = (id) => {
       const newBlogs = blogs.filter(blog => blog.id !== id);
       setblogs(newBlogs)
@@ -18,15 +16,11 @@ const Home = () => {
 
     useEffect(() => {
       console.log('use effect ran')
-      console.log(blogs)
-      console.log(name)
-    }, [name]);
+    }, []);
 
     return ( 
         <div className="home">
           <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
-          <button onClick={() => setName('Luigi')}>change name</button>
-          <p>{name}</p>
         </div>
      );
 }
