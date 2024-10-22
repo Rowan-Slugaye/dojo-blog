4be210097e169b8ca63a962a0ad00a93aecdd5c0
Rowan-Slugaye <83877730+Rowan-Slugaye@users.noreply.github.com>
2024-10-22 16:08:50 +0200

    EP 18 Finshed

--- src/home.js
+++ src/home.js
@@ -4,20 +4,25 @@ import BlogList from "./BlogList";
 const Home = () => {
 
     const [blogs, setblogs] = useState(null)
+    const [isPending, setIsPending] = useState(true)
 
     useEffect(() => {
+      setTimeout(() => {
       console.log('use effect ran')
-      fetch('http://192.168.86.166:8000/blogs')
+      fetch('http://192.168.86.166:8000/blogs') // I didnt use http://localhost:8000/blogs becuse i wanted it to work on other Computers 
        .then(res => {
         return res.json()})
         .then((data) => {
           console.log(data)
           setblogs(data)
+          setIsPending(false)
         })
+      }, 1000)
     }, []);
 
     return ( 
         <div className="home">
+          { isPending && <div>Loading...</div>}
           {blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
         </div>
      );
