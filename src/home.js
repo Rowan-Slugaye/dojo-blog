14c2470f036aa54d05f88e6fd10b6acd0b0948af
Rowan-Slugaye <83877730+Rowan-Slugaye@users.noreply.github.com>
2024-10-28 18:14:04 +0100

    EP 20 done WOOOOOOW

--- src/home.js
+++ src/home.js
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-
+ const { data:blogs, isPending, error} = useFetch("http://192.168.86.166:8000/blogs")  // I didnt use http://localhost:8000/blogs becuse i wanted it to display on a diffrent pc so i could both see the video, program and see the site update in real time 
     
 
     
