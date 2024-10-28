063a763169203a7331160161d11ef0ffb0bd9884
Rowan-Slugaye <83877730+Rowan-Slugaye@users.noreply.github.com>
2024-10-28 22:15:29 +0100

    half way

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,31 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+
     return ( 
         <div className="create">
             <h2>Add a Neew Blog</h2>
+            <form >
+                <label>Blog title:</label>
+                <input 
+                type="text"
+                required 
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Blog body:</label>
+                <textarea
+                required
+                ></textarea>
+                <label>Blog auther:</label>
+                <select>
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+                <button>add blog</button>
+                <p>{ title }</p>
+            </form>
         </div>
      );
 }
