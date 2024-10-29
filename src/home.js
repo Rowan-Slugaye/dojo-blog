54cb5bc1cd553ef7697ab12dd186be0653ff2cc5
Rowan-Slugaye <83877730+Rowan-Slugaye@users.noreply.github.com>
2024-10-29 17:55:37 +0100

    Started EP 28 pluss fixed useFetch when it isn't run on my pc

--- src/home.js
+++ src/home.js
@@ -2,7 +2,7 @@ import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
 const Home = () => {
- const { data:blogs, isPending, error} = useFetch(process.env.REACT_APP_API_URL || "http://localhost:8000/blogs")  // I didnt use http://localhost:8000/blogs becuse i wanted it to display on a diffrent pc so i could both see the video, program and see the site update in real time 
+ const { data:blogs, isPending, error} = useFetch(process.env.REACT_APP_API_URL || "http://localhost:8000/blogs")  //  this now chooses Localhost:8000 if you dont have REACT_APP_API_URL defined in .env.local file
     
 
     
