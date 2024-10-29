6f3826b84d30e0a8a454e29b70ba83240e2a9f16
Rowan-Slugaye <83877730+Rowan-Slugaye@users.noreply.github.com>
2024-10-29 18:34:52 +0100

    half  way

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,20 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
+import { useHistory } from "react-router-dom";
 
 const BlogDetails = () => {
     const { id } = useParams()
     const { data: blog, error, isPending} = useFetch(`${process.env.REACT_APP_API_URL || "http://localhost:8000/blogs"}/${id}`);
+    const history = useHistory();
+
+    const handleClick= () => {
+        fetch(`${process.env.REACT_APP_API_URL || "http://localhost:8000/blogs"}/${blog.id}`, {
+            method: 'DELETE'
+        }).then (() => {
+            history.push('/');
+        })
+    }
+
 
     return ( 
         <div className="blog-details">
@@ -14,6 +25,7 @@ const BlogDetails = () => {
                     <h2>{ blog.title }</h2>
                     <p>Written by { blog.author }</p>
                     <div>{ blog.body }</div>
+                    <button onClick={handleClick}>delete</button>
                 </article>
             )}
         </div>
