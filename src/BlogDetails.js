522a6692e20ccdcd9a5165dd8fc656bbc8aa2d69
Rowan-Slugaye <83877730+Rowan-Slugaye@users.noreply.github.com>
2024-10-29 13:08:43 +0100

    fixed it so it works better on not only my pc

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -3,7 +3,7 @@ import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams()
-    const { data: blog, error, isPending} =  useFetch("http://192.168.86.166:8000/blogs/" + id);
+    const { data: blog, error, isPending} = useFetch(`${process.env.REACT_APP_API_URL || "http://localhost:8000/blogs"}/${id}`);
 
     return ( 
         <div className="blog-details">
--- src/home.js
+++ src/home.js
@@ -2,7 +2,7 @@ import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
 const Home = () => {
- const { data:blogs, isPending, error} = useFetch("http://192.168.86.166:8000/blogs")  // I didnt use http://localhost:8000/blogs becuse i wanted it to display on a diffrent pc so i could both see the video, program and see the site update in real time 
+ const { data:blogs, isPending, error} = useFetch(process.env.REACT_APP_API_URL || "http://localhost:8000/blogs")  // I didnt use http://localhost:8000/blogs becuse i wanted it to display on a diffrent pc so i could both see the video, program and see the site update in real time 
     
 
     
